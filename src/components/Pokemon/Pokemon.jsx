cbcb8a796a464fa7d8c9c9891b6295b85dbce147
Manishsahusahu <sahumanish327@gmail.com>
2023-08-15 21:30:38 +0530

    Fetching and display of pokemons

---
+++ src/components/Pokemon/Pokemon.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Pokemon = ({ name, image }) => {
+  return (
+    <div>
+      <div>{name}</div>
+      <div>
+        <img src={image} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Pokemon;
--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./PokemonList.css";
+import Pokemon from "../Pokemon/Pokemon";
 
 const PokemonList = () => {
-  return (
-    <div>PokemonList</div>
-  )
-}
+  const [PokemonList, setPokemonList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-export default PokemonList
+  async function downloadData() {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    const pokemonResultsPromises = response.data.results.map((result) =>
+      axios.get(result.url)
+    );
+    const pokemonsData = await axios.all(pokemonResultsPromises);
+    const res = pokemonsData.map((pokedata) => {
+      const pokemon = pokedata.data;
+      return {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: pokemon.sprites.other.dream_world.front_default,
+        types: pokemon.types,
+      };
+    });
+    console.log(res);
+    setPokemonList(res);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    downloadData();
+  }, []);
+
+  return (
+    <div className="pokemon-list-wrapper">
+      <div>PokemonList</div>
+      {isLoading ? "Is loading..." : PokemonList.map((pokemon) => <Pokemon key={pokemon.id} name={pokemon.name} image={pokemon.image} />)}
+    </div>
+  );
+};
+
+export default PokemonList;
